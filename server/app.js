3249dc38fb84b93f291b15987b6aae5eb0a19ee8
Login Viktor <batazor111@gmail.com>
2016-04-21 17:24:59 +0300

    chore(backend): server-side render

--- server/app.js
+++ server/app.js
@@ -2,19 +2,70 @@
 
 import path from 'path'
 import express from 'express'
+import serialize from 'serialize-javascript'
+
+import webpack from 'webpack'
+import webpackDevMiddleware from 'webpack-dev-middleware'
+import webpackConfig from '../webpack.config'
+
+import React from 'react'
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux'
+import { renderToString } from 'react-dom/server'
+import { Provider } from 'react-redux'
+import { Route, IndexRoute, Link, createMemoryHistory, match, RouterContext } from 'react-router'
+import { routerReducer, routerMiddleware, syncHistoryWithStore } from 'react-router-redux'
+
+import { configureStore, DevTools } from '../src/store'
+import routes from '../src/routes'
+
 import { app as configApp } from '../config'
 
 let app = express()
 
-app.set('view', path.resolve(__dirname, './view'))
-app.set('view engine', 'jade')
+app.use(webpackDevMiddleware(webpack(webpackConfig), {
+  publicPath: '/__build__/',
+  stats: {
+    colors: true
+  }
+}))
 
-app.use(express.static(path.resolve(__dirname, '../bundle')))
+const HTML = ({ content, store }) => (
+  <html>
+    <body>
+      <div id="app" dangerouslySetInnerHTML={{ __html: content }}/>
+      <div id="devtools"/>
+      <script dangerouslySetInnerHTML={{ __html: `window.__initialState__=${serialize(store.getState())};` }}/>
+      <script src="/__build__/bundle.js"/>
+    </body>
+  </html>
+)
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../public/template.html'));
+app.use(function (req, res) {
+  const memoryHistory = createMemoryHistory(req.url)
+  const store = configureStore(memoryHistory)
+  const history = syncHistoryWithStore(memoryHistory, store)
+
+  match({ history, routes, location: req.url }, (error, redirectLocation, renderProps) => {
+    if (error) {
+      res.status(500).send(error.message)
+    } else if (redirectLocation) {
+      res.redirect(302, redirectLocation.pathname + redirectLocation.search)
+    } else if (renderProps) {
+      const content = renderToString(
+        <Provider store={store}>
+          <RouterContext {...renderProps}/>
+        </Provider>
+      )
+
+      console.log(content)
+
+      res.send('<!doctype html>\n' + renderToString(<HTML content={content} store={store}/>))
+    }
+  })
 })
 
+app.use(express.static(path.resolve(__dirname, '../dist')))
+
 app.listen(configApp.APP_PORT, () => {
-  console.log(`Server run on port ${ configApp.APP_PORT }`);
+  console.log(`Server listening on http://localhost:${ configApp.APP_PORT }, Ctrl+C to stop`);
 });
--- src/app.js
+++ src/app.js
@@ -1,7 +1,3 @@
-import { createDevTools } from 'redux-devtools'
-import LogMonitor from 'redux-devtools-log-monitor'
-import DockMonitor from 'redux-devtools-dock-monitor'
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, combineReducers } from 'redux'
@@ -9,39 +5,23 @@ import { Provider } from 'react-redux'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
 
-import * as reducers from './reducers'
+import { configureStore, DevTools } from './store'
+import routes from './routes'
 import { App, Home, Foo, Bar } from './components'
 
-const reducer = combineReducers({
-  ...reducers,
-  routing: routerReducer
-})
-
-const DevTools = createDevTools(
-  <DockMonitor toggleVisibilityKey="ctrl-h" changePositionKey="ctrl-q">
-    <LogMonitor theme="tomorrow" preserveScrollTop={false} />
-  </DockMonitor>
-)
-
-const store = createStore(
-  reducer,
-  DevTools.instrument()
-)
-
+const store = configureStore(browserHistory, window.__initialState__)
 const history = syncHistoryWithStore(browserHistory, store)
 
 ReactDOM.render(
   <Provider store={store}>
-    <div>
-      <Router history={history}>
-        <Route path="/" component={App}>
-          <IndexRoute component={Home} />
-          <Route path="foo" component={Foo} />
-          <Route path="bar" component={Bar} />
-        </Route>
-      </Router>
-      <DevTools />
-    </div>
+    <Router history={history} routes={routes} />
   </Provider>,
   document.getElementById('app')
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <DevTools/>
+  </Provider>,
+  document.getElementById('devtools')
 )
